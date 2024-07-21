7ec03f6d5d0b7086b8df46c6e8c0435cd29d008e
NirmalDudhat <nirmaldudhat12@gmail.com>
2024-07-21 17:58:33 +0530

    Commit

--- src/sorting/SelectionSorting.java
+++ src/sorting/SelectionSorting.java
@@ -1,7 +1,31 @@
 package sorting;
 
 public class SelectionSorting {
-    public static void main(String[] args) {
+    public static void selectionSort(int[] arry){
+        for (int i = 0; i < arry.length; i++) {
+            int minPos = i;
+            for (int j = i+1; j < arry.length; j++) {
+                if (arry[minPos] > arry[j]) {
+                    minPos = j;
+                }
+            }
+            int tmp = arry[minPos];
+            arry[minPos] = arry[i];
+            arry[i] = tmp;
+        }
+    }
 
+    public static void printArray(int[] arry){
+        for (int j : arry) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5,4,1,3,2};
+
+        selectionSort(array);
+        printArray(array);
     }
 }
