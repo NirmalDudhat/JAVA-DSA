7dc7c6220dfe62191d67b368e09883958f1846b3
NirmalDudhat <nirmaldudhat12@gmail.com>
2024-08-02 22:18:14 +0530

    Commit

---
+++ src/twoDArray/DiagonalSum.java
@@ -0,0 +1,45 @@
+package twoDArray;
+
+public class DiagonalSum {
+
+    public static int diagonalSum(int[][] matrix){
+        int sum = 0;
+
+        //Primary Diagonal
+        /*for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i == j) {
+                    sum += matrix[i][j];
+                }
+                else if (i + j == matrix.length-1) {
+                    sum += matrix[i][j];
+                }
+            }
+        }*/
+
+        for (int i = 0; i < matrix.length; i++) {
+            sum += matrix[i][i];
+
+            if (i != matrix.length-1-i){
+                sum += matrix[i][matrix.length-i-1];
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+//                {1,2,3,4},
+//                {5,6,7,8},
+//                {9,10,11,12},
+//                {13,14,15,16}
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        System.out.println(matrix.length);
+
+        System.out.println(diagonalSum(matrix));
+    }
+}
